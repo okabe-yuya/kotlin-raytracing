41d870df83c27dc7a5f69cf653efc065b46753df
okabe-yuya <takamizawa46@gmail.com>
2024-07-21 16:45:18 +0900

    feat: Colorクラスを定義
    描画を担当するクラスを定義しました。

---
+++ app/src/main/kotlin/org/example/Color.kt
@@ -0,0 +1,18 @@
+package org.example
+
+import org.example.Vec3
+
+typealias Color = Vec3
+
+fun writeColor(pixelColor: Color) {
+    val r = pixelColor.x
+    val g = pixelColor.y
+    val b = pixelColor.z
+
+    val rByte: Int = (255.999 * r).toInt()
+    val gByte: Int = (255.999 * g).toInt()
+    val bByte: Int = (255.999 * b).toInt()
+
+    print("${rByte} ${gByte} ${bByte}")
+}
+
